98fd49584fe1eba557aaa08c275e2b018be17c1a
Randy Steele <steelerx155@gmail.com>
2019-10-23 19:21:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand, :BRANDS 
+  attr_reader :brand, 
   
   BRANDS = []
 
@@ -15,6 +15,6 @@ class Shoe
 
 def brand=(brand)
   @brand = brand
-  BRANDS << brand
+  BRANDS << @brand
  end
 end 
